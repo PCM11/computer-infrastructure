42e065d45007fabd77914a817c7bcd8f62e55c46
PCM11 <157810147+PCM11@users.noreply.github.com>
2024-12-01 23:35:20 +0000

    Github actions generated by ChatGPT

--- weather_data.py
+++ weather_data.py
@@ -1,12 +1,16 @@
-# Weather data
-
+# Libraries
+import pandas as pd
 #Dates and times
 import datetime as dt
 
+# Load data
+df = pd.read_json('https://prodapi.metweb.ie/observations/athenry/today')
 # Get the current date and time.
 filename = dt.datetime.now()
 
 # Create a string from the current date and time.
-filename = filename.strftime("%Y-%m-%d_%H-%M-%S") +".json"
+filename = filename.strftime("%Y%m%d_%H%M%S") +".json"
+filename = 'data/' + filename
 
-filename 
+# Save data to json file
+df.to_json(filename)
